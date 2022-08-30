ad85826d1d45dbd7676c419b58f21a9ebf941cfe
Simi Prambos <simi.prambos@gmail.com>
2022-08-30 16:58:28 +0700

    feat: add translation

--- config/voyager.php
+++ config/voyager.php
@@ -127,16 +127,16 @@ return [
     'dashboard' => [
         // Add custom list items to navbar's dropdown
         'navbar_items' => [
-            'voyager::generic.profile' => [
-                'route'      => 'voyager.profile',
-                'classes'    => 'class-full-of-rum',
-                'icon_class' => 'voyager-person',
-            ],
-            'voyager::generic.home' => [
-                'route'        => '/',
-                'icon_class'   => 'voyager-home',
-                'target_blank' => true,
-            ],
+            // 'voyager::generic.profile' => [
+            //     'route'      => 'voyager.profile',
+            //     'classes'    => 'class-full-of-rum',
+            //     'icon_class' => 'voyager-person',
+            // ],
+            // 'voyager::generic.home' => [
+            //     'route'        => '/',
+            //     'icon_class'   => 'voyager-home',
+            //     'target_blank' => true,
+            // ],
             'voyager::generic.logout' => [
                 'route'      => 'voyager.logout',
                 'icon_class' => 'voyager-power',
@@ -183,7 +183,7 @@ return [
 
     'primary_color' => '#22A7F0',
 
-    'show_dev_tips' => true, // Show development tip "How To Use:" in Menu and Settings
+    'show_dev_tips' => false, // Show development tip "How To Use:" in Menu and Settings
 
     // Here you can specify additional assets you would like to be included in the master.blade
     'additional_css' => [
